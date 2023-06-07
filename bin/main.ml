fb5b3838951069cde13350784eb7ae24b86fab45
csuraparaju <csuraparaju@wpi.edu>
2023-06-07 17:10:57 -0400

    literals polished (no hash structs yet)

--- bin/main.ml
+++ bin/main.ml
@@ -1,7 +1,6 @@
 open Cwrap
 
    let rec repl () =
-  print_string "Welcome to the CML repl! Refer to https://github.com/csuraparaju/CML for docs! \n" ;
   print_string ">> ";
   flush stdout;
   let line = read_line () in
@@ -13,4 +12,4 @@ open Cwrap
           | Some t -> print_endline (Token.string_of_token_type t); traverse lex'
     in traverse lexbuf; repl ()
 
-let _ = repl () ;;
+let _ = print_string "Welcome to the CML repl! Refer to https://github.com/csuraparaju/CML for docs! \n" ; repl () ;;
